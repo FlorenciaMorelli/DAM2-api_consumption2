f3112b136546a713f635d6f9b3608ad1c87a917a
Florencia Morelli <florenciamorelliIT@gmail.com>
2024-08-30 20:47:35 -0300

    Removed unused imports

--- app/src/main/java/com/example/api_consumption2/MainActivity.kt
+++ app/src/main/java/com/example/api_consumption2/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.api_consumption2
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.api_consumption2.adapter.ProductAdapter
 import com.example.api_consumption2.databinding.ActivityMainBinding
