6f434fa6d81aec867bda99f5584b916089ec67be
Florencia Morelli <florenciamorelliIT@gmail.com>
2024-08-30 20:11:25 -0300

    Add internet usage

--- app/src/main/java/com/example/api_consumption2/MainActivity.kt
+++ app/src/main/java/com/example/api_consumption2/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.example.api_consumption2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.api_consumption2.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        getListProducts()
+
+    }
+
+    fun getListProducts(){
+        CoroutineScope(Dispatchers.IO).launch {
+            val callApi = RetrofitClient.api.getProducts()
+            runOnUiThread{
+                if (callApi.isSuccessful){
+                    val products = callApi.body()
+                    Log.d("ProductApi", "Lista de productos: $products")
+                } else {
+                    Log.d("ProductApi", "No se obtuvo respuesta de la pegada")
+                }
+            }
         }
     }
 }
